7ef1617d7971be4fe5788a5bf6919c4042f2914f
Tommy Chen <tommy351@gmail.com>
2016-11-14 12:32:23 +0800

    Do not create cluster if it's created

--- 3.2/start.sh
+++ 3.2/start.sh
@@ -30,6 +30,8 @@ supervisord
 sleep 3
 
 # Create Redis cluster
-echo "yes" | /redis-trib.rb create --replicas 1 127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005
+if [ -z "/data/redis-7000/nodes.conf" ]; then
+  echo "yes" | /redis-trib.rb create --replicas 1 127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005
+fi
 
 tail -f $log_dir/*.log
