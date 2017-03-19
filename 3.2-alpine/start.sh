6c93e8deda0f7f18e6695865db2a42fa15519491
Rueian Oneecom <rueiancsie@gmail.com>
2017-03-20 00:27:54 +0800

    Bind redis on 0.0.0.0 instead of 127.0.0.1

--- 3.2-alpine/start.sh
+++ 3.2-alpine/start.sh
@@ -7,6 +7,11 @@ lock_file=/data/cluster.lock
 
 mkdir -p $log_dir
 
+redis_host="0.0.0.0"
+if [ -n "$REDIS_HOSTNAME" ]; then
+  echo "0.0.0.0   $REDIS_HOSTNAME" >> /etc/hosts
+fi
+
 # Initialize configs
 for p in 7000 7001 7002 7003 7004 7005
 do
@@ -33,7 +38,7 @@ sleep 3
 # Create Redis cluster
 if [ ! -f $lock_file ]; then
   touch $lock_file
-  echo "yes" | /redis-trib.rb create --replicas 1 127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005
+  echo "yes" | /redis-trib.rb create --replicas 1 "$redis_host":7000 "$redis_host":7001 "$redis_host":7002 "$redis_host":7003 "$redis_host":7004 "$redis_host":7005
 fi
 
 tail -f $log_dir/*.log
